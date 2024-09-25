5499f4a31f2682027ba7b14ffc6e048c1473383f
Thiraet Wanlaem <looy2300@gmail.com>
2024-09-25 21:05:44 +0700

    first commit

---
+++ script2.js
@@ -0,0 +1,15 @@
+// แสดงภาพในโมดอลเมื่อกดปุ่ม "ดูรูปภาพ"
+document.getElementById('viewImageBtn').addEventListener('click', function() {
+    let modal = document.getElementById('imageModal');
+    let modalImg = document.getElementById('modalImage');
+    
+    // เปิดโมดอลและแสดงภาพตัวอย่าง
+    modal.style.display = "block";
+    modalImg.src = "22.png";  // แทนที่ด้วย URL ของภาพตัวอย่าง
+});
+
+// ปิดโมดอลเมื่อกดปุ่มปิด (x)
+document.getElementsByClassName('close')[0].addEventListener('click', function() {
+    console.log("close");
+    document.getElementById('imageModal').style.display = "none";
+});
